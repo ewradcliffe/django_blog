1d34e3c3dd855ef9022f3f24d5de3895afdb7b23
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-07-19 08:31:21 +0000

    Add code prior to initial deployment.

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-iffr$o+c^oi)ay)bbd&d=)y0fq@02#sxfi0taukg-*2&9^7g9c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ewradcliffe-djangoblog-wvlgnt8gavx.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-ewradcliffe-djangoblog-wvlgnt8gavx.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
