5e1f033dd8fcb80ec6db48b9eefcd2cc07ee02db
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-07-22 10:44:25 +0000

    Add basic view template.

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["8000-ewradcliffe-djangoblog-wvlgnt8gavx.ws.codeinstitute-ide.net", '.herokuapp.com']
 
