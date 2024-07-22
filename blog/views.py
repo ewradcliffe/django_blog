e954897516e5d34f8c8c23e863c7c7e0b3f3e359
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-07-22 12:02:32 +0000

    Update view filter code to only show published posts.

--- blog/views.py
+++ blog/views.py
@@ -9,5 +9,5 @@ from .models import Post
 #    return HttpResponse('Hello Blog!')
 
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
     template_name = "post_list.html"
