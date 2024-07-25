1c5b901cc91698beda71f6f9c20e73f28ded04c1
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-07-25 10:20:57 +0000

    Add cloudinary to about section.

--- about/models.py
+++ about/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 class About(models.Model):
     title = models.CharField(max_length=200)
+    profile_image = CloudinaryField('image', default='placeholder')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
 
@@ -19,3 +21,4 @@ class CollaborateRequest(models.Model):
 
     def __str__(self):
         return f"Collaboration request from {self.name}"
+
